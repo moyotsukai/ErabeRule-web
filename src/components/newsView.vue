733a0968f1144f7ca7c37affb32879ebba366df3
Shinchan-git <gyoshu.bowie@gmail.com>
2021-05-18 17:32:33 +0900

    11個以上でバグがあることを表記

--- src/components/newsView.vue
+++ src/components/newsView.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="news-app" class="body-padding">
     <div class="home-content">
+      <p class="page-title">現在確認しているバグ</p>
+      <div class="home-section">
+        <ul class="text-left">
+          <div class="news">
+            <li class="primary-text">
+              ボルダルールまたはコンドルセルールで、選択肢を11個以上入力すると投票の結果が表示できないバグが発生しています。ご不便をおかけしていますが、現在修正に取り組んでいます。
+            </li>
+          </div>
+        </ul>
+      </div>
+
       <p class="page-title">リリースノート</p>
       <div class="home-section">
         <ul class="text-left">
-
           <div class="news">
             <p class="supporting-text">2021/04/12</p>
             <li class="primary-text">
               マジョリティ・ジャッジメント(Majority Judgement)を追加しました。
-              <br>
+              <br />
               マジョリティ・ジャッジメントを採用して投票ルームを作成する場合、評価を表す語彙(「非常に良い」「不十分」など)をカスタマイズすることもできます。
             </li>
           </div>
@@ -18,7 +28,7 @@
             <p class="supporting-text">2021/03/09</p>
             <li class="primary-text">
               「結果の検証を表示」ボタンの追加。
-              <br>
+              <br />
               結果の画面で、他の投票ルールを採用していたらどのような結果になっていたか、シュミレーションできます。
               ボルダルールまたはコンドルセ・ヤングの最尤法で集計されたルームのみで表示されます。
             </li>
@@ -26,9 +36,10 @@
 
           <div class="news">
             <p class="supporting-text">2021/03/07</p>
-            <li class="primary-text">投票済みの人数が正しく表示されないことがあるバグを修正。</li>
+            <li class="primary-text">
+              投票済みの人数が正しく表示されないことがあるバグを修正。
+            </li>
           </div>
-
         </ul>
       </div>
     </div>
